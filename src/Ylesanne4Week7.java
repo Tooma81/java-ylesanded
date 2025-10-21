69a32d6ab09d9b7ba4e31006bab2f1f0d16b62c5
Tooma81 <toomas.kaarel@gmail.com>
2025-10-21 13:25:45 +0300

    Ylesanne 4 A lot of bricks

--- src/Ylesanne4Week7.java
+++ src/Ylesanne4Week7.java
@@ -1,21 +1,16 @@
 public class Ylesanne4Week7 {
-    public static void main(String[] args) {
-        Thing book = new Thing("Happiness in Three Steps", 2);
-        Thing mobile = new Thing("Nokia 3210", 1);
-        Thing brick = new Thing("Brick", 4);
-
-        Suitcase tomsCase = new Suitcase(10);
-        tomsCase.addThing(book);
-        tomsCase.addThing(mobile);
-
-        Suitcase georgesCase = new Suitcase(10);
-        georgesCase.addThing(brick);
-
+    public static void main(String[] Container) {
         Container container = new Container(1000);
-        container.addSuitcase(tomsCase);
-        container.addSuitcase(georgesCase);
+        addSuitcasesFullOfBricks(container);
+        System.out.println(container);
+    }
 
-        System.out.println("There are the following things in the container suitcases:");
-        container.printThings();
+    public static void addSuitcasesFullOfBricks(Container container) {
+        Thing brick = new Thing("Brick", 4);
+        for (int i = 0; i < 100; i++) {
+            Suitcase suitcase = new Suitcase(10);
+            suitcase.addThing(brick);
+            container.addSuitcase(suitcase);
+        }
     }
 }
