4ec347e0f88c65f2cab5c6d8848f47a8eba10979
Tooma81 <toomas.kaarel@gmail.com>
2025-10-22 09:03:45 +0300

    Ylesanne 7 Dictionary

---
+++ src/Dictionary.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Dictionary {
+
+    private HashMap<String, String> translation;
+
+    public Dictionary() {
+        this.translation = new HashMap<String, String>();
+    }
+
+    public void add(String finnish, String english) {
+        this.translation.put(finnish, english);
+    }
+
+    public String translate(String finnish) {
+        return this.translation.get(finnish);
+    }
+}
---
+++ src/Ylesanne7Week7.java
@@ -0,0 +1,11 @@
+public class Ylesanne7Week7 {
+    public static void main(String[] Dictionary) {
+        Dictionary dictionary = new Dictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("cembalo", "harpsichord");
+
+        System.out.println(dictionary.translate("apina"));
+        System.out.println(dictionary.translate("porkkana"));
+    }
+}
