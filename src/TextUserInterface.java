8464c2b7dd1c59d0442b35d99672b51d03d1eba5
Tooma81 <toomas.kaarel@gmail.com>
2025-10-28 14:16:17 +0200

    Ylesanne 7 Adding or translating words

--- src/TextUserInterface.java
+++ src/TextUserInterface.java
@@ -11,14 +11,31 @@ public class TextUserInterface {
     }
 
     public void start() {
+        System.out.println("Statements:\n" +
+                "  add - adds a word pair to the dictionary\n" +
+                "  translate - asks a word and prints its translation\n" +
+                "  quit - quits the text user interface");
         while (true) {
             System.out.print("Statement: ");
             String input = this.reader.nextLine().toLowerCase();
-            if (input.equals("quit")) {
-                System.out.println("Cheers!");
-                break;
-            } else {
-                System.out.println("Unknown statement");
+            switch(input) {
+                case "add":
+                    System.out.print("In Finnish: ");
+                    String finnish = reader.nextLine();
+                    System.out.print("Translation: ");
+                    String english = reader.nextLine();
+                    this.dictionary.add(finnish, english);
+                    break;
+                case "translate":
+                    System.out.print("Give a word: ");
+                    String word = reader.nextLine();
+                    this.dictionary.translate(word);
+                    break;
+                case "quit":
+                    System.out.println("Cheers!");
+                    return;
+                default:
+                    System.out.println("Unknown statement");
             }
         }
     }
