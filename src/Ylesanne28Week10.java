ef7f3c3cfccdfbd3c48d6edca90bf8309c3a434f
Tooma81 <toomas.kaarel@gmail.com>
2025-10-30 17:14:35 +0200

    Ylesanne 28 Implementing organism

---
+++ src/Ylesanne28Week10.java
@@ -0,0 +1,13 @@
+import movable.Organism;
+
+public class Ylesanne28Week10 {
+    public static void main(String[] Organism) {
+        Organism organism = new Organism(20, 30);
+        System.out.println(organism);
+        organism.move(-10, 5);
+        System.out.println(organism);
+        organism.move(50, 20);
+        System.out.println(organism);
+
+    }
+}
