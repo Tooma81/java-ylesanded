29cc1a16c9f40d8128c9792e6b78015e6812dcfc
Tooma81 <toomas.kaarel@gmail.com>
2025-11-06 11:07:54 +0200

    Ylesanne 30 ContainerHistory Phase 3

--- src/containers/ContainerHistory.java
+++ src/containers/ContainerHistory.java
@@ -26,11 +26,29 @@ public class ContainerHistory {
     }
 
     public double average() {
-        int sum = 0;
-        for (int i=0; i< history.size(); i++) {
-            sum += i;
+        double sum = 0;
+        for (Double situation : history) {
+            sum += situation;
         }
-        return !history.isEmpty() ? (double) sum / history.size() : 0;
+        return !history.isEmpty() ? sum / history.size() : 0;
+    }
+
+    public double greatestFluctuation() {
+        double greatestFluctuation = 0.0;
+        for (int i = 0; i < history.size(); i++) {
+            double fluctuation = Math.abs(history.get(i + 1) - history.get(1));
+            greatestFluctuation = Math.max(greatestFluctuation, fluctuation);
+        }
+        return history.size() > 1 ? greatestFluctuation : 0;
+    }
+
+    public double variance() {
+        double avg = average();
+        double squaredDiffsSum = 0.0;
+        for (Double situation : history) {
+            squaredDiffsSum += Math.pow(situation - avg, 2);
+        }
+        return !history.isEmpty() ? squaredDiffsSum / history.size() : 0;
     }
 
     public String toString() {
